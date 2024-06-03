0d27b0f56a1b190592fd768d8767cdf777b32614
TheHolyRaj <ryanclarke319@yahoo.co.uk>
2024-06-03 18:38:46 +0100

    math now attacks correctly

--- src/main/java/net/theholyraj/rajswordmod/world/item/custom/KnockbackSwordItem.java
+++ src/main/java/net/theholyraj/rajswordmod/world/item/custom/KnockbackSwordItem.java
@@ -39,7 +39,7 @@ public class KnockbackSwordItem extends SwordItem {
      //   pLivingEntity.getBoundingBox().getV
     }
 
-    public void attack(Level pLevel, LivingEntity pLivingEntity){
+    public void attack(Level pLevel, LivingEntity pLivingEntity) {
         Vec3 rightVector;
         Vec3 upVector;
         Vec3 viewVector;
@@ -80,155 +80,98 @@ public class KnockbackSwordItem extends SwordItem {
         // Adjust the position to place the nearest face of the cuboid in front of the player
         Vec3 frontCenter = position.add(viewVector.scale(length / 2.0 + halfLength));
 
-        // Calculate the eight corners of the cuboid
-        Vec3 topFrontLeft = frontCenter.add(leftVector.scale(halfWidth)).add(upVector.scale(halfHeight));
-        Vec3 topFrontRight = frontCenter.add(rightVector.scale(halfWidth)).add(upVector.scale(halfHeight));
-        Vec3 bottomFrontLeft = frontCenter.add(leftVector.scale(halfWidth)).add(downVector.scale(halfHeight));
-        Vec3 bottomFrontRight = frontCenter.add(rightVector.scale(halfWidth)).add(downVector.scale(halfHeight));
-        Vec3 topBackLeft = frontCenter.add(leftVector.scale(halfWidth)).add(upVector.scale(halfHeight)).add(viewVector.scale(-length));
-        Vec3 topBackRight = frontCenter.add(rightVector.scale(halfWidth)).add(upVector.scale(halfHeight)).add(viewVector.scale(-length));
-        Vec3 bottomBackLeft = frontCenter.add(leftVector.scale(halfWidth)).add(downVector.scale(halfHeight)).add(viewVector.scale(-length));
-        Vec3 bottomBackRight = frontCenter.add(rightVector.scale(halfWidth)).add(downVector.scale(halfHeight)).add(viewVector.scale(-length));
+        // Length of each smaller cuboid
+        double smallLength = length / 3.0;
+        double smallHalfLength = smallLength / 2.0;
 
+        // Calculate front centers for each smaller cuboid
+        Vec3 frontCenter1 = frontCenter.add(viewVector.scale(-length / 2.0 + smallHalfLength));
+        Vec3 frontCenter2 = frontCenter1.add(viewVector.scale(smallLength));
+        Vec3 frontCenter3 = frontCenter2.add(viewVector.scale(smallLength));
 
-        pLevel.addParticle(ParticleTypes.BUBBLE,topFrontLeft.x, topFrontLeft.y, topFrontLeft.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.BUBBLE,topFrontRight.x, topFrontRight.y, topFrontRight.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.BUBBLE,bottomFrontLeft.x, bottomFrontLeft.y, bottomFrontLeft.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.BUBBLE,bottomFrontRight.x, bottomFrontRight.y, bottomFrontRight.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.BUBBLE,topBackLeft.x, topBackLeft.y, topBackLeft.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.BUBBLE,topBackRight.x, topBackRight.y, topBackRight.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.BUBBLE,bottomBackLeft.x, bottomBackLeft.y, bottomBackLeft.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.BUBBLE,bottomBackRight.x, bottomBackRight.y, bottomBackRight.z, 0, 0, 0);
+        // Calculate the eight corners of the three smaller cuboids
+        Vec3[] cuboid1Corners = calculateCorners(frontCenter1, smallLength, halfWidth, halfHeight, leftVector, rightVector, upVector, downVector, viewVector);
+        Vec3[] cuboid2Corners = calculateCorners(frontCenter2, smallLength, halfWidth, halfHeight, leftVector, rightVector, upVector, downVector, viewVector);
+        Vec3[] cuboid3Corners = calculateCorners(frontCenter3, smallLength, halfWidth, halfHeight, leftVector, rightVector, upVector, downVector, viewVector);
 
+        for (Vec3 corner : cuboid1Corners){
+            addParticle(pLevel, corner);
+        }
+        for (Vec3 corner : cuboid2Corners){
+            addParticle(pLevel, corner);
+        }
+        for (Vec3 corner : cuboid3Corners){
+            addParticle(pLevel, corner);
+        }
 
-        Vec3 vectorFromCuboid = new Vec3(topFrontLeft.x - topBackLeft.x
-                ,topFrontLeft.y - topBackLeft.y
-                ,topFrontLeft.z - topBackLeft.z);
+        // Create AABB instances for each smaller cuboid
+        AABB aabb1 = createAABBFromCorners(cuboid1Corners);
+        AABB aabb2 = createAABBFromCorners(cuboid2Corners);
+        AABB aabb3 = createAABBFromCorners(cuboid3Corners);
 
-        Vec3 thirdVectorFromCuboid = new Vec3(vectorFromCuboid.x/3
-                ,vectorFromCuboid.y/3
-                ,vectorFromCuboid.z/3);
-
-        Vec3 firstPosTopLeft1 = topBackLeft;
-        Vec3 firstPosTopRight1 = topBackRight;
-        Vec3 firstPosBottomLeft1 = bottomBackLeft;
-        Vec3 firstPosBottomRight1 = bottomBackRight;
-
-        Vec3 firstPosTopLeft2 = new Vec3(firstPosTopLeft1.x * thirdVectorFromCuboid.x
-                , firstPosTopLeft1.y * thirdVectorFromCuboid.y
-                , firstPosTopLeft1.z * thirdVectorFromCuboid.z) ;
-        Vec3 firstPosTopRight2 = new Vec3(firstPosTopRight1.x * thirdVectorFromCuboid.x
-                , firstPosTopRight1.y * thirdVectorFromCuboid.y
-                , firstPosTopRight1.z * thirdVectorFromCuboid.z) ;
-        Vec3 firstPosBottomLeft2 = new Vec3(firstPosBottomLeft1.x * thirdVectorFromCuboid.x
-                , firstPosBottomLeft1.y * thirdVectorFromCuboid.y
-                , firstPosBottomLeft1.z * thirdVectorFromCuboid.z) ;
-        Vec3 firstPosBottomRight2 = new Vec3(firstPosBottomRight1.x * thirdVectorFromCuboid.x
-                , firstPosBottomRight1.y * thirdVectorFromCuboid.y
-                , firstPosBottomRight1.z * thirdVectorFromCuboid.z) ;
-        pLevel.addParticle(ParticleTypes.SMOKE,firstPosTopLeft2.x,firstPosTopLeft2.y,firstPosTopLeft2.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.SMOKE,firstPosTopRight2.x,firstPosTopRight2.y,firstPosTopRight2.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.SMOKE,firstPosBottomLeft2.x,firstPosBottomLeft2.y,firstPosBottomLeft2.z, 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.SMOKE,firstPosBottomRight2.x,firstPosBottomRight2.y,firstPosBottomRight2.z, 0, 0, 0);
-
-
-        Vec3 firstPosTopLeft3 = new Vec3(firstPosTopLeft2.x * thirdVectorFromCuboid.x
-                , firstPosTopLeft2.y * thirdVectorFromCuboid.y
-                , firstPosTopLeft2.z * thirdVectorFromCuboid.z) ;
-        Vec3 firstPosTopRight3 = new Vec3(firstPosTopRight2.x * thirdVectorFromCuboid.x
-                , firstPosTopRight2.y * thirdVectorFromCuboid.y
-                , firstPosTopRight2.z * thirdVectorFromCuboid.z) ;
-        Vec3 firstPosBottomLeft3 = new Vec3(firstPosBottomLeft2.x * thirdVectorFromCuboid.x
-                , firstPosBottomLeft2.y * thirdVectorFromCuboid.y
-                , firstPosBottomLeft2.z * thirdVectorFromCuboid.z) ;
-        Vec3 firstPosBottomRight3 = new Vec3(firstPosBottomRight2.x * thirdVectorFromCuboid.x
-                , firstPosBottomRight2.y * thirdVectorFromCuboid.y
-                , firstPosBottomRight2.z * thirdVectorFromCuboid.z) ;
-
-        Vec3 firstPosTopLeft4 = topFrontLeft;
-        Vec3 firstPosTopRight4 = topFrontRight;
-        Vec3 firstPosBottomLeft4 = bottomFrontLeft;
-        Vec3 firstPosBottomRight4 = bottomFrontRight;
-
-        AABB aabb1 = makeAABB(firstPosTopLeft1, firstPosTopRight1, firstPosBottomLeft1, firstPosBottomRight1
-                , firstPosTopLeft2, firstPosTopRight2 ,firstPosBottomLeft2, firstPosBottomRight2);
-        AABB aabb2 = makeAABB(firstPosTopLeft2, firstPosTopRight2, firstPosBottomLeft2, firstPosBottomRight2
-                , firstPosTopLeft3, firstPosTopRight3 ,firstPosBottomLeft3, firstPosBottomRight3);
-        AABB aabb3 = makeAABB(firstPosTopLeft3, firstPosTopRight3, firstPosBottomLeft3, firstPosBottomRight3
-                , firstPosTopLeft4, firstPosTopRight4 ,firstPosBottomLeft4, firstPosBottomRight4);
-       List<Entity> list = pLevel.getEntities(pLivingEntity, aabb1);
-       for (Entity entity : list){
-           entity.hurt(pLevel.damageSources().drown(), 4);
-       }
+        List<Entity> list1 = pLevel.getEntities(pLivingEntity, aabb1);
         List<Entity> list2 = pLevel.getEntities(pLivingEntity, aabb2);
-        for (Entity entity : list2){
-            entity.hurt(pLevel.damageSources().drown(), 4);
-        }
         List<Entity> list3 = pLevel.getEntities(pLivingEntity, aabb3);
-        for (Entity entity : list3){
-            entity.hurt(pLevel.damageSources().drown(), 4);
+        addParticle(pLevel, aabb1);
+        addParticle(pLevel, aabb2);
+        addParticle(pLevel, aabb3);
+
+        for (Entity entity : list1) {
+            entity.hurt(pLevel.damageSources().playerAttack((Player) pLivingEntity), 6);
+            pLivingEntity.sendSystemMessage(Component.literal("hit"));
+        }
+        for (Entity entity : list2) {
+            entity.hurt(pLevel.damageSources().playerAttack((Player) pLivingEntity), 6);
+            pLivingEntity.sendSystemMessage(Component.literal("hit2"));
+        }
+        for (Entity entity : list3) {
+            entity.hurt(pLevel.damageSources().playerAttack((Player) pLivingEntity), 6);
+            pLivingEntity.sendSystemMessage(Component.literal("hit3"));
         }
     }
 
-    public AABB makeAABB(Vec3 pos1, Vec3 pos2, Vec3 pos3, Vec3 pos4, Vec3 pos5, Vec3 pos6, Vec3 pos7, Vec3 pos8){
-        double x1 = returnLowestNumber(pos1.x, pos2.x, pos3.x, pos4.x, pos5.x, pos6.x, pos7.x, pos8.x);
-        double y1 = returnLowestNumber(pos1.y, pos2.y, pos3.y, pos4.y, pos5.y, pos6.y, pos7.y, pos8.y);
-        double z1 = returnLowestNumber(pos1.z, pos2.z, pos3.z, pos4.z, pos5.z, pos6.z, pos7.z, pos8.z);
-        double x2 = returnLowestNumber(pos1.x, pos2.x, pos3.x, pos4.x, pos5.x, pos6.x, pos7.x, pos8.x);
-        double y2 = returnHighestNumber(pos1.y, pos2.y, pos3.y, pos4.y, pos5.y, pos6.y, pos7.y, pos8.y);
-        double z2 = returnHighestNumber(pos1.z, pos2.z, pos3.z, pos4.z, pos5.z, pos6.z, pos7.z, pos8.z);
-
-        return new AABB(x1,y1,z1,x2,y2,z2);
+    AABB createAABBFromCorners(Vec3[] corners) {
+        double minX = Double.POSITIVE_INFINITY, minY = Double.POSITIVE_INFINITY, minZ = Double.POSITIVE_INFINITY;
+        double maxX = Double.NEGATIVE_INFINITY, maxY = Double.NEGATIVE_INFINITY, maxZ = Double.NEGATIVE_INFINITY;
+        for (Vec3 corner : corners) {
+            if (corner.x < minX) minX = corner.x;
+            if (corner.y < minY) minY = corner.y;
+            if (corner.z < minZ) minZ = corner.z;
+            if (corner.x > maxX) maxX = corner.x;
+            if (corner.y > maxY) maxY = corner.y;
+            if (corner.z > maxZ) maxZ = corner.z;
+        }
+        return new AABB(minX, minY, minZ, maxX, maxY, maxZ);
     }
 
-    public double returnLowestNumber(double a, double b, double c, double d, double e, double f, double g, double h){
-        if (a < b && a < c && a < d && a < e && a < f && a < g && a < h){
-            return a;
-        }
-        else if (b < c && b < d && b < e && b < f && b < g && b < h){
-            return b;
-        }
-        else if (c < d && c < e && c < f && c < g && c < h){
-            return c;
-        }
-        else if (d < e && d < f && d < g && d < h){
-            return d;
-        }
-        else if (e < f && e < g && e < h){
-            return e;
-        }
-        else if (f < g && f < h){
-            return f;
-        }
-        else if (g < h){
-            return g;
-        }
-        else return h;
+    Vec3[] calculateCorners(Vec3 center, double length, double halfWidth, double halfHeight, Vec3 leftVector, Vec3 rightVector, Vec3 upVector, Vec3 downVector, Vec3 viewVector) {
+        Vec3[] corners = new Vec3[8];
+        Vec3 halfLengthVec = viewVector.scale(length / 2.0);
+        corners[0] = center.add(leftVector.scale(halfWidth)).add(upVector.scale(halfHeight)).add(halfLengthVec);
+        corners[1] = center.add(rightVector.scale(halfWidth)).add(upVector.scale(halfHeight)).add(halfLengthVec);
+        corners[2] = center.add(leftVector.scale(halfWidth)).add(downVector.scale(halfHeight)).add(halfLengthVec);
+        corners[3] = center.add(rightVector.scale(halfWidth)).add(downVector.scale(halfHeight)).add(halfLengthVec);
+        corners[4] = center.add(leftVector.scale(halfWidth)).add(upVector.scale(halfHeight)).subtract(halfLengthVec);
+        corners[5] = center.add(rightVector.scale(halfWidth)).add(upVector.scale(halfHeight)).subtract(halfLengthVec);
+        corners[6] = center.add(leftVector.scale(halfWidth)).add(downVector.scale(halfHeight)).subtract(halfLengthVec);
+        corners[7] = center.add(rightVector.scale(halfWidth)).add(downVector.scale(halfHeight)).subtract(halfLengthVec);
+        return corners;
     }
+    public void addParticle(Level pLevel, Vec3 vec3){
+        pLevel.addParticle(ParticleTypes.BUBBLE,vec3.x, vec3.y, vec3.z, 0, 0, 0);
 
-    public double returnHighestNumber(double a, double b, double c, double d, double e, double f, double g, double h){
-        if (a > b && a > c && a > d && a > e && a > f && a > g && a > h){
-            return a;
-        }
-        else if (b > c && b > d && b > e && b > f && b > g && b > h){
-            return b;
-        }
-        else if (c > d && c > e && c > f && c > g && c > h){
-            return c;
-        }
-        else if (d > e && d > f && d > g && d > h){
-            return d;
-        }
-        else if (e > f && e > g && e > h){
-            return e;
-        }
-        else if (f > g && f > h){
-            return f;
-        }
-        else if (g > h){
-            return g;
-        }
-        else return h;
+    }
+    public void addParticle(Level pLevel, AABB aabb){
+        pLevel.addParticle(ParticleTypes.FLAME,aabb.minX, aabb.minY, aabb.minZ, 0, 0, 0);
+        pLevel.addParticle(ParticleTypes.FLAME,aabb.minX, aabb.maxY, aabb.minZ, 0, 0, 0);
+        pLevel.addParticle(ParticleTypes.FLAME,aabb.minX, aabb.minY, aabb.maxZ, 0, 0, 0);
+        pLevel.addParticle(ParticleTypes.FLAME,aabb.minX, aabb.maxY, aabb.maxZ, 0, 0, 0);
+
+        pLevel.addParticle(ParticleTypes.FLAME,aabb.maxX, aabb.minY, aabb.minZ, 0, 0, 0);
+        pLevel.addParticle(ParticleTypes.FLAME,aabb.maxX, aabb.maxY, aabb.minZ, 0, 0, 0);
+        pLevel.addParticle(ParticleTypes.FLAME,aabb.maxX, aabb.minY, aabb.maxZ, 0, 0, 0);
+        pLevel.addParticle(ParticleTypes.FLAME,aabb.maxX, aabb.maxY, aabb.maxZ, 0, 0, 0);
+
     }
 
     @Override
